c5ba17a8fe0a33107d6bedc9568e150c63579c44
Jeremy GALKOWSKI <jeremy.galkowski@nowwweb.com>
2025-09-03 14:11:09 +0200

    Test deploiement

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,29 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    '@storybook/addon-a11y',
-    "@storybook/addon-docs"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  },
-  viteFinal: (config) => {
-    // Assurez-vous que `base` est bien configuré pour fonctionner avec GitHub Pages
-    config.base = '/storybook-project/'; // Remplace par ton propre dépôt
-    return config;
-  }
+    "stories": [
+        "../src/**/*.mdx",
+        "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-onboarding",
+        "@chromatic-com/storybook",
+        '@storybook/addon-a11y',
+        "@storybook/addon-docs"
+    ],
+    "framework": {
+        "name": "@storybook/vue3-vite",
+        "options": {}
+    },
+    async viteFinal(config, { configType }) {
+        // Applique le chemin de base uniquement pour le build de PRODUCTION
+        if (configType === 'PRODUCTION') {
+            return mergeConfig(config, {
+                base: '/storybook-project/',
+            });
+        }
+        return config;
+    }
 };
 export default config;
